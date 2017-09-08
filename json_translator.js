c88b05c6ccff19e35a81bab54674c388dd1a9b1a
kayandrewj <kayandrewj@gmail.com>
2017-09-08 16:13:31 -0400

    finish links, fix adding duplicate nodes to graph notation

--- json_translator.js
+++ json_translator.js
@@ -1,18 +1,45 @@
+let links = [];
+let parentNode = {};
 let nodes = [];
 
-function mapArticlesToNodes() {
-  let nodes = [];
+let nodeIndex = {};
+
+function parseJSON() {
+  if (!parentNode.name) {
+    parentNode = {
+      name: parentObj.parse.title,
+      depth: 0
+    };
+  }
+
+  parseArticlesToNodes();
+  parseNodesToLinks();
+  console.log(nodes);
+  console.log(links);
+}
+
+function parseArticlesToNodes() {
   let articles = linksArr;
+  let newNodes = [];
 
   articles.forEach(article => {
-    let node = { name: ''};
-    node.name = article['*'];
-    nodes.push(node);
+    let node = {
+      name: article['*'],
+      depth: parentNode.depth + 1
+    };
+    if (!(node.name in nodeIndex)) {
+      nodes.push(node);
+    }
+    nodeIndex[node.name] = true;
   });
-
 }
-  //
-  // let demo = {
-  //   target: parentObj.parse.title,
-  //   source: linksArr[i]['*']
-  // };
+
+function parseNodesToLinks() {
+  nodes.forEach(node => {
+    let link = {
+      source: parentNode.name,
+      target: node.name,
+    };
+    links.push(link);
+  });
+}
