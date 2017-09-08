1d6f47091bb3e412dc3f3b139b08218525c78d78
kayandrewj <kayandrewj@gmail.com>
2017-09-07 21:31:48 -0400

    fix rejected api calls

--- api_util.js
+++ api_util.js
@@ -1,63 +1,50 @@
-let linksObj = {};
 let parentObj = {};
+let linksArr = [];
 let linksGraph = {};
 
-// takes text from input, sends generated URL and callback to getSections xml request
 function handleSearch(query) {
   getSections(buildSectionUrl(query), prepareGetLinks);
 }
 
 function storeLinks(links) {
-  linksObj = links;
+  linksArr = links.parse.links;
+  debugger
 }
 
 function storeParent(json) {
   parentObj = json;
 }
 
-// builds url to fetch all sections of target article
 function buildSectionUrl(query) {
   return `https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=sections&page=${query}`;
 }
 
-// sends request to get article section json
-// callback: calls `prepareGetLinks`
 function getSections(url, callback) {
-  let xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        callback(xmlHttp.responseJson);
-      }
-    };
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: callback
+  });
 }
-
-// saves parent article for access in link builder
-// finds index of 'see also' section
-// calls getLinks xml request, sending final link storage action as callback.
 
 function prepareGetLinks(json) {
   storeParent(json);
   let sectionArr = json.parse.sections;
   sectionArr.forEach((obj) => {
     if (obj.anchor === "See_also") {
-      getLinks(buildLinksUrl(obj.index), storeLinks);
+      getLinks(buildLinksUrl(parseInt(obj.index)));
     }
   });
 }
 
 function buildLinksUrl(index) {
-  return `https://en.wikipedia.org/w/api.php?action=parse&prop=links&page=${parentObj.parse.title}&section=${index}`;
+  return `https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=links&page=${parentObj.parse.title}&section=${index}`;
 }
 
-function getLinks(url, callback) {
-  let xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        callback(xmlHttp.responseJson);
-      }
-    };
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
+function getLinks(url) {
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+  }).then(links => storeLinks(links));
+
 }
