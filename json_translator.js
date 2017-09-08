3ce64e449f2ec13492748ac2c24abc03da1665cd
kayandrewj <kayandrewj@gmail.com>
2017-09-08 14:07:20 -0400

    build initial json/node translator

--- json_translator.js
+++ json_translator.js
@@ -1,14 +1,18 @@
-function mapArticlesToNodes() {
-  let nodes;
-  
+let nodes = [];
 
+function mapArticlesToNodes() {
+  let nodes = [];
+  let articles = linksArr;
+
+  articles.forEach(article => {
+    let node = { name: ''};
+    node.name = article['*'];
+    nodes.push(node);
+  });
 
 }
-
-
-
-
-  let demo = {
-    target: parentObj.parse.title,
-    source: linksArr[i]['*']
-  };
+  //
+  // let demo = {
+  //   target: parentObj.parse.title,
+  //   source: linksArr[i]['*']
+  // };
