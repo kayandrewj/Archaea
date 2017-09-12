7455e3af66467dd7aaff77a94516a7bb3e6dbfef
kayandrewj <kayandrewj@gmail.com>
2017-09-12 17:47:56 -0400

    create preview article for initial parent node

--- search.js
+++ search.js
@@ -1,8 +1,12 @@
+let Eve = "";
+
 document.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('searchform').addEventListener("submit", (e) => {
     e.preventDefault();
+    Eve = e.target["0"].value;
     handleSearch(e.target["0"].value);
+    data(dataGraph);
   });
 
   document.getElementById('reset').onclick = () => {
@@ -18,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       links: [],
     };
     data(dataGraph);
-    document.getElementById('article-prev-title').innerHTML = "How to use Archaea";
+    document.getElementById('article-prev-title').innerHTML = "How to Use Archaea";
     document.getElementById('article-prev-text').innerHTML = "Search for anything or click the demo button. Then pull nodes to grow the article tree. If new nodes don't sprout, you've reached the end of that branch!";
 
     document.getElementById('article-link').href = `https://github.com/kayandrewj/Archaea/blob/master/README.md`;
@@ -26,3 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
 });
+
+
+//what does it mean when an element in the chrome console turns purple?
