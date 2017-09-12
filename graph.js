829522f542326ad761355b8fd7065aae55f4c532
kayandrewj <kayandrewj@gmail.com>
2017-09-12 17:46:53 -0400

    fix reset => no rerender bug, nodes didn't stay active and graph went to sleep

--- graph.js
+++ graph.js
@@ -12,7 +12,7 @@ const canvas = d3.select("#network"),
        .strength(25))
     .force("y", d3.forceY(height/2)
       .strength(25))
-    .force("collide", d3.forceCollide(r+3))
+    .force("collide", d3.forceCollide(r+4))
     .force("charge", d3.forceManyBody()
       .strength(-50))
     .force("linkStrength", d3.forceLink()
@@ -41,6 +41,7 @@ function data(graph) {
     canvas.height = window.innerHeight;
 
     simulation
+      .alpha(0.05)
       .force("center", d3.forceCenter(width/2, height/2))
       .force("x", d3.forceX(canvas.width/2))
       .force("y", d3.forceY(canvas.height/2));
@@ -62,54 +63,57 @@ function data(graph) {
     return simulation.find(d3.event.x, d3.event.y);
   }
 
+  if (document.getElementById('article-prev-title').innerHTML === "How to Use Archaea") {
+    getArticlePreview(Eve);
+  }
 }
 
-  function drawNode(d) {
-    ctx.beginPath();
-    ctx.fillStyle = color(d.depth);
-    ctx.moveTo(d.x, d.y);
-    ctx.arc(d.x, d.y, r, 0, 2*Math.PI);
-    ctx.fill();
+function drawNode(d) {
+  ctx.beginPath();
+  ctx.fillStyle = color(d.depth);
+  ctx.moveTo(d.x, d.y);
+  ctx.arc(d.x, d.y, r, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+function drawLink(l) {
+  ctx.moveTo(l.source.x, l.source.y);
+  ctx.lineTo(l.target.x, l.target.y);
+}
+
+function showTitle() {
+  console.log(d3.event.subject.name);
+}
+
+
+function dragStarted() {
+  getArticlePreview(d3.event.subject.name);
+  let articleLink = null;
+  let drawArticleLink = () => {
+    ctx.moveTo(d3.event.subject.x, d3.event.subject.y);
+    ctx.lineTo(0, 0);
+  };
+  drawArticleLink();
+
+  dragTimer = 0;
+  if (!d3.event.active)
+  simulation.alphaTarget(0.3).restart();
+  d3.event.subject.fx = d3.event.subject.x;
+  d3.event.subject.fy = d3.event.subject.y;
+}
+
+function dragged() {
+  dragTimer += 1;
+  if (dragTimer === 8) {
+    handleSearch(d3.event.subject.name);
   }
 
-  function drawLink(l) {
-    ctx.moveTo(l.source.x, l.source.y);
-    ctx.lineTo(l.target.x, l.target.y);
-  }
+  d3.event.subject.fx = d3.event.x;
+  d3.event.subject.fy = d3.event.y;
+}
 
-  function showTitle() {
-    console.log(d3.event.subject.name);
-  }
-
-
-  function dragStarted() {
-    getArticlePreview(d3.event.subject.name);
-    let articleLink = null;
-    let drawArticleLink = () => {
-      ctx.moveTo(d3.event.subject.x, d3.event.subject.y);
-      ctx.lineTo(0, 0);
-    };
-    drawArticleLink();
-
-    dragTimer = 0;
-    if (!d3.event.active)
-    simulation.alphaTarget(0.3).restart();
-    d3.event.subject.fx = d3.event.subject.x;
-    d3.event.subject.fy = d3.event.subject.y;
-  }
-
-  function dragged() {
-    dragTimer += 1;
-    if (dragTimer === 8) {
-      handleSearch(d3.event.subject.name);
-    }
-
-    d3.event.subject.fx = d3.event.x;
-    d3.event.subject.fy = d3.event.y;
-  }
-
-  function dragEnded() {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d3.event.subject.fx = null;
-    d3.event.subject.fy = null;
-  }
+function dragEnded() {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d3.event.subject.fx = null;
+  d3.event.subject.fy = null;
+}
